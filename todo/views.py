af7642e77c4cc72b7181db8f5189a815ee3e7c58
CharOConnell <charlotteoconnell@virginmedia.com>
2020-05-21 09:38:52 +0000

    Add a new item adds to the database

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Item
 
 # Create your views here.
@@ -7,4 +7,12 @@ def get_todo_list(request):
     return render(request, "todo_list.html", {'items': results})
 
 def create_an_item(request):
+    if request.method=="POST":
+        new_item = Item() # new instance of the item model
+        new_item.name = request.POST.get('name')
+        new_item.done = 'done' in request.POST # for a checkbox
+        new_item.save() # saves item to database
+
+        return redirect(get_todo_list)
+
     return render(request, "item_form.html")
