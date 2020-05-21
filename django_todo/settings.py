c89b096979391d3612a72d32716888236de3227e
CharOConnell <charlotteoconnell@virginmedia.com>
2020-05-21 12:24:48 +0000

    PostgreSQL database should be installed

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,13 +77,20 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+    'default':
+    dj_database_url.parse(
+        "postgres://bcvsrjeoyktixk:ac5f97fc091d055cf6b62d6540d75c6737f76148a6d1bb3f40c94b\
+        5e0ee7c46e@ec2-54-228-251-117.eu-west-1.compute.amazonaws.com:5432/dfqk5bi\
+        gcjqnpg")
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
